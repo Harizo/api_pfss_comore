c512804adfb907438373137f7babc070e2801662
aime_bruno <ndrianaina.aime.bruno@gmail.com>
2021-05-05 09:55:00 +0300

    05/05/2021

---
+++ application/controllers/api/Theme_formation.php
@@ -0,0 +1,123 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Theme_formation extends REST_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('theme_formation_model', 'ThemeformationManager');
+    }
+
+    public function index_get() {
+        $id = $this->get('id');
+		$data = array();
+		if ($id) {
+			// Selection par id
+			$temporaire = $this->ThemeformationManager->findById($id);
+			if($temporaire) {
+				$data=$temporaire;
+			}
+		} else {
+			// Selection de tous les enregistrements	
+			$temporaire = $this->ThemeformationManager->findAll();
+			if ($temporaire) {
+				$data=$temporaire;
+			}
+		}
+        if (count($data)>0) {
+            $this->response([
+                'status' => TRUE,
+                'response' => $data,
+                'message' => 'Get data success',
+            ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => FALSE,
+                'response' => array(),
+                'message' => 'No data were found'
+            ], REST_Controller::HTTP_OK);
+        }
+    }
+    public function index_post() {
+        $id = $this->post('id') ;
+        $supprimer = $this->post('supprimer') ;
+		$data = array(
+			'description' => $this->post('description'),
+		);               
+        if ($supprimer == 0) {
+            if ($id == 0) {
+                if (!$data) {
+                    $this->response([
+                        'status' => FALSE,
+                        'response' => 0,
+                        'message' => 'No request found'
+                            ], REST_Controller::HTTP_BAD_REQUEST);
+                }
+				// Ajout d'un enregitrement
+                $dataId = $this->ThemeformationManager->add($data);              
+                if (!is_null($dataId)) {
+                    $this->response([
+                        'status' => TRUE,
+                        'response' => $dataId,
+                        'message' => 'Data insert success'
+                            ], REST_Controller::HTTP_OK);
+                } else {
+                    $this->response([
+                        'status' => FALSE,
+                        'response' => 0,
+                        'message' => 'No request found'
+                            ], REST_Controller::HTTP_BAD_REQUEST);
+                }
+            } else {
+                if (!$data || !$id) {
+                    $this->response([
+                        'status' => FALSE,
+                        'response' => 0,
+                        'message' => 'No request found'
+                            ], REST_Controller::HTTP_BAD_REQUEST);
+                }
+				// Mise à jour d'un enregistrement
+                $update = $this->ThemeformationManager->update($id, $data);              
+                if(!is_null($update)){
+                    $this->response([
+                        'status' => TRUE, 
+                        'response' => 1,
+                        'message' => 'Update data success'
+                            ], REST_Controller::HTTP_OK);
+                } else {
+                    $this->response([
+                        'status' => FALSE,
+                        'message' => 'No request found'
+                            ], REST_Controller::HTTP_OK);
+                }
+            }
+        } else {
+            if (!$id) {
+            $this->response([
+            'status' => FALSE,
+            'response' => 0,
+            'message' => 'No request found'
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }
+			// Suppression d'un enregitrement
+            $delete = $this->ThemeformationManager->delete($id);          
+            if (!is_null($delete)) {
+                $this->response([
+                    'status' => TRUE,
+                    'response' => 1,
+                    'message' => "Delete data success"
+                        ], REST_Controller::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => FALSE,
+                    'response' => 0,
+                    'message' => 'No request found'
+                        ], REST_Controller::HTTP_OK);
+            }
+        }   
+    }
+}
+?>
